9ba891462eb82983c36bf51113d08587faf59a08
Stephanie Asmar <sasmar8@gmail.com>
2017-07-12 15:42:32 -0400

    JavaScript practice examples

---
+++ main.js
@@ -0,0 +1,82 @@
+// // console.log("Hello World");
+
+// function printThing() {
+//     console.log('a thing');
+//     return 'a thing';
+// }
+
+// function greet(whom, what) {
+//     if (!whom) {
+//         whom = 'Darth Carl';
+//     }
+//     return 'oh hai, ' + what + ' to you, ' + whom;
+// }
+
+// function hello(name) {
+//     if (!name) {
+//         return name;
+//     } else if {
+//         return 'Hello' + ' ' + name;
+//     }
+// }
+
+// function fave(name, sub) {
+//     return name + 's' + ' ' + 'favorite subject in school is' + ' ' + sub;
+// }
+
+
+// the code below works!
+// function tipAmount(bill, serviceLevel) {
+//     if (serviceLevel == 'good') {
+//         return bill * .2;
+//     } else if (serviceLevel == 'fair') {
+//         return bill * .15;
+//     } else if (serviceLevel == 'poor') {
+//         return bill * .1;
+//     } else {
+//         return 'No Tip for you!'
+//     }
+// }
+
+
+// // the code below works!
+// function totalAmount(bill, serviceLevel) {
+//     if (serviceLevel == 'good') {
+//         return (bill * .2) + bill;
+//     } else if (serviceLevel == 'fair') {
+//         return (bill * .15) + bill;
+//     } else if (serviceLevel == 'poor') {
+//         return (bill * .1) + bill;
+//     } else {
+//         return 'No Tip for you!'
+//     }
+// }
+
+// // the code below works!
+// function splitAmount(bill, serviceLevel, numPeople) {
+//     if (serviceLevel == 'good') {
+//         return ((bill * .2) + bill) / numPeople;
+//     } else if (serviceLevel == 'fair') {
+//         return ((bill * .15) + bill) / numPeople;
+//     } else if (serviceLevel == 'poor') {
+//         return ((bill * .1) + bill) / numPeople;
+//     } else {
+//         return 'No Tip for you!'
+//     }
+// }
+
+// // the code below works!
+// function printNumbers(i, i) {
+//     while (i > 0) {
+//         return(i);
+//         i--;
+//     }
+// }
+
+function printNum(i, i) {
+    for i in printNum {
+        if i > 0;
+        return(i);
+        i--;
+    }
+}
